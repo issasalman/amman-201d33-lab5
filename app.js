2f38b26bec8322d4c1621f5cb8ac8c5063fbee62
issasalman <issasalmanhu1996@gmail.com>
2021-07-08 13:41:39 +0300

    test

--- app.js
+++ app.js
@@ -93,7 +93,10 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+function multiplyArray(multArr) { 
+  
+  
+  test//eslint-disable-line
 
 }
 
@@ -121,7 +124,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+function multiplyAnyArray(dynamicArray) {   
+  test//eslint-disable-line
 
 }
 
