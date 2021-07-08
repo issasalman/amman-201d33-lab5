d8c7da4420aa13c49b61d7889412d66388e422d7
issasalman <issasalmanhu1996@gmail.com>
2021-07-08 16:17:51 +0300

    s

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a,b,c) { //eslint-disable-line
   let result=a*b;
   return [result,`The product of ${a} and ${b} is ${result}.`]
 }
@@ -50,12 +50,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a,b,c){
+  let sum1=sum(a,b)[0];
+      sum1=sum(sum1,c)[0]
+  let multi=multiply(a,b,c)[0];
+      multi=multiply(multi,c)[0];
+  return[sum1,multi,`${a} and ${b} and ${c} sum to ${sum1}.`,`The product of ${a} and ${b} and ${c} is ${multi}.`]
+
 
 }
-
+console.log(sumAndMultiply())
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
