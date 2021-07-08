4ad690f37f9dd6a6825aa3646d63366007073c82
issasalman <issasalmanhu1996@gmail.com>
2021-07-08 14:19:29 +0300

    problem3

--- app.js
+++ app.js
@@ -50,12 +50,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) {
+   //eslint-disable-line
+   let summation=sum(a,b,c)[0];
+   let multiplii=multiply(a,b,c)[0];
+   return[summation,multiplii,`${a} and ${b} and ${c} sum to ${summation}.`,`The product of ${a} and ${b} and ${c} is ${multiplii}.`];
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
+console.log(sumAndMultiply(3,6,7))
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
